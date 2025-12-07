e898f51e467f5cd45cae3b0fde4682f1cf6f314d
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2025-12-07 20:25:46 +0200

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,31 +6,24 @@ const BASE_URL = "https://gravo-hutadev.vercel.app"
 
 export const metadata: Metadata = {
   title: "Gravo Business Solution | Soluções Digitais & SaaS para Crescimento Empresarial",
-  description:
-    "Plataforma digital innovadora para elevar a experiência de Negócios. Criamos soluções SaaS user-centric, prontas para o futuro, que impulsionam a productividade e o crescimento das emprêzas.",
-  keywords:
-    "Gravo, Business Solution, SaaS, Soluções Digitais, Design User-Centric, Desenvolvimento Web, Productividade, Tecnologia, Python, JavaScript, Next.js",
+  description: "Plataforma digital innovadora para elevar a experiência de Negócios. Criamos soluções SaaS user-centric, prontas para o futuro, que impulsionam a productividade e o crescimento das emprêzas.",
+  keywords: "Gravo, Business Solution, SaaS, Soluções Digitais, Design User-Centric, Desenvolvimento Web, Productividade, Tecnologia, Python, JavaScript, Next.js",
   authors: [{ name: "Hutauta Developer (HutaDev)" }],
   creator: "Hutauta Developer (HutaDev)",
   publisher: "Hutauta Developer (HutaDev)",
   robots: "index, follow",
 
   metadataBase: new URL(BASE_URL),
-  manifest: "/site.webmanifest",
+  manifest: "https://gravo-hutadev.vercel.app/site.webmanifest",
 
   icons: {
-    icon: [
-      { url: `${BASE_URL}/favicon.ico` },
-      { url: `${BASE_URL}/favicon.svg`, type: "image/svg+xml" },
-      { url: `${BASE_URL}/favicon-96x96.png`, type: "image/png" }
-    ],
+    icon: [{ url: `${BASE_URL}/favicon.ico` }, { url: `${BASE_URL}/favicon.svg`, type: "image/svg+xml" }, { url: `${BASE_URL}/favicon-96x96.png`, type: "image/png" }],
     apple: [{ url: `${BASE_URL}/apple-touch-icon.png`, sizes: "180x180" }]
   },
 
   openGraph: {
     title: "Gravo Business Solution | Plataforma Digital Innovadora",
-    description:
-      "Criamos plataformas SaaS user-centric que transformam negócios, simplificam operações e aceleram o crescimento. O seu parceiro em soluções digitais technológicas.",
+    description: "Criamos plataformas SaaS user-centric que transformam negócios, simplificam operações e aceleram o crescimento. O seu parceiro em soluções digitais technológicas.",
     url: BASE_URL,
     siteName: "Gravo Business Solution",
     type: "website",
@@ -48,8 +41,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Gravo Business Solution | Soluções para Negócios & Vida",
-    description:
-      "SaaS e Soluções Digitais de ponta para optimizar a gestão e productividade. Desenvolvido com Python, Next.js e React.js.",
+    description: "SaaS e Soluções Digitais de ponta para optimizar a gestão e productividade. Desenvolvido com Python, Next.js e React.js.",
     images: [`${BASE_URL}/demo.webp`]
   },
 
